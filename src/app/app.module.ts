8f007e3e7fd1e5fed74fa0f4dfcf8079c910f96d
tebogomoloi <tebogo@eightyfour600.com>
2020-01-23 19:47:28 +0200

    feat: slick carousel styles with css grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
@@ -12,6 +13,7 @@ import { FeedComponent } from './components/feed/feed.component';
 import { ExploreComponent } from './components/explore/explore.component';
 import { ReadLaterComponent } from './components/read-later/read-later.component';
 import { FeedChannelComponent } from './components/feed-channel/feed-channel.component';
+import { FeedArticleComponent } from './components/feed-article/feed-article.component';
 
 @NgModule({
   declarations: [
@@ -21,16 +23,19 @@ import { FeedChannelComponent } from './components/feed-channel/feed-channel.com
     FeedComponent,
     ExploreComponent,
     ReadLaterComponent,
-    FeedChannelComponent
+    FeedChannelComponent,
+    FeedArticleComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    SlickCarouselModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [FeedArticleComponent]
 })
 export class AppModule { }
