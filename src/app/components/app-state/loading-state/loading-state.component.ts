5191d278554cfb9e2794f75ba9b0fb3826f315bc
tebogomoloi <tebogo@eightyfour600.com>
2020-01-24 19:00:46 +0200

    feature: created spinner components

---
+++ src/app/components/app-state/loading-state/loading-state.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-state',
+  templateUrl: './loading-state.component.html',
+  styleUrls: ['./loading-state.component.scss']
+})
+export class LoadingStateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
