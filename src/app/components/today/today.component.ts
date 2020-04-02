5cb190c99ae5acad4440e0629c9a41736875ebc5
Tebogo Moloi <tebogo@eightyfour600.com>
2020-04-02 21:28:12 +0200

    fix: slick slider config and css

--- src/app/components/today/today.component.ts
+++ src/app/components/today/today.component.ts
@@ -25,14 +25,14 @@ export class TodayComponent implements OnInit {
     slidesToShow: 5,
     slidesToScroll: 2,
     infinite: false,
+    mobileFirst: true,
+    arrows: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          infinite: true,
-          dots: true
         }
       },
       {
@@ -44,6 +44,13 @@ export class TodayComponent implements OnInit {
       },
       {
         breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 260,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
